2b38f8991f6a9bbaca6d147817caf25505833e78
KristenKarcher <kristenmkarcher@gmail.com>
2016-06-13 00:24:33 -0400

    Checkpoint 38 authentication: completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   resources :topics do
 
-    resources :posts, except: [:index]
-  end
+  resources :posts, except: [:index]
+end
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
 
   get 'about' => 'welcome#about'
