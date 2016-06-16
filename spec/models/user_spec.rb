f8684c9575653f5d6d2f365853a944cfe87a1205
KristenKarcher <kristenmkarcher@gmail.com>
2016-06-16 00:10:36 -0400

    Checkpoint 41 comments: completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+
    it { is_expected.to have_many(:posts) }
+   it { is_expected.to have_many(:comments) }
    # Shoulda tests for name
    it { is_expected.to validate_presence_of(:name) }
    it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -17,69 +19,66 @@ RSpec.describe User, type: :model do
    it { is_expected.to validate_presence_of(:password) }
    it { is_expected.to have_secure_password }
    it { is_expected.to validate_length_of(:password).is_at_least(6) }
-end
-
-  describe "attributes" do
-  #   it "should have name and email attributes" do
-  #     expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-  end
-
-  describe "invalid user" do
-    let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-    let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-
-    it "should be an invalid user due to blank name" do
-      expect(user_with_invalid_name).to_not be_valid
-    end
-
-    it "should be an invalid user due to blank email" do
-      expect(user_with_invalid_email).to_not be_valid
-    end
-
-    it "responds to role" do
-       expect(user).to respond_to(:role)
-     end
 
 
-     it "responds to admin?" do
-       expect(user).to respond_to(:admin?)
-     end
-
-
-     it "responds to member?" do
-       expect(user).to respond_to(:member?)
-     end
-   end
-
-   describe "roles" do
-
-     it "is member by default" do
-       expect(user.role).to eq("member")
-     end
-
-
-     context "member user" do
-       it "returns true for #member?" do
-         expect(user.member?).to be_truthy
+   describe "attributes" do
+       it "should have name and email attributes" do
+         expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
        end
 
-       it "returns false for #admin?" do
-         expect(user.admin?).to be_falsey
+       it "responds to role" do
+         expect(user).to respond_to(:role)
+       end
+
+       it "responds to admin?" do
+         expect(user).to respond_to(:admin?)
+       end
+
+       it "responds to member?" do
+         expect(user).to respond_to(:member?)
        end
      end
 
-
-     context "admin user" do
-       before do
-         user.admin!
+     describe "roles" do
+       it "is member by default" do
+         expect(user.role).to eq("member")
        end
 
-       it "returns false for #member?" do
-         expect(user.member?).to be_falsey
+       context "member user" do
+         it "returns true for #member?" do
+           expect(user.member?).to be_truthy
+         end
+
+         it "returns false for #admin?" do
+           expect(user.admin?).to be_falsey
+         end
        end
 
-       it "returns true for #admin?" do
-         expect(user.admin?).to be_truthy
+       context "admin user" do
+         before do
+           user.admin!
+         end
+
+         it "returns false for #member?" do
+           expect(user.member?).to be_falsey
+         end
+
+         it "returns true for #admin" do
+           expect(user.admin?).to be_truthy
+         end
+       end
+     end
+
+     describe "invalid user" do
+       let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+       let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+
+       it "should be an invalid user due to blank name" do
+         expect(user_with_invalid_name).to_not be_valid
+       end
+
+       it "should be an invalid user due to blank email" do
+         expect(user_with_invalid_email).to_not be_valid
        end
      end
    end
