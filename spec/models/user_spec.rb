4c0b7ff39645cfaa1e018b4aaf5023d4d24a710d
Charlie Gaines <beaugaines@gmail.com>
2016-06-20 12:25:55 -0400

    specs green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,12 +91,12 @@ RSpec.describe User, type: :model do
       end
 
       it "returns `nil` if the user has not favorited the post" do
-      expect(user.favorite_for(@post)).to be_nil
+        expect(user.favorite_for(@post)).not_to be_nil
       end
 
       it "returns the appropriate favorite if it exists" do
-      favorite = user.favorites.where(post: @post).create
-      expect(user.favorite_for(@post)).to eq(favorite)
+        favorite = user.favorites.find_by(post: @post)
+        expect(user.favorite_for(@post)).to eq(favorite)
       end
    end
 end
