4d683516d961bb32a41d985bf220f9ae033e163d
KristenKarcher <kristenmkarcher@gmail.com>
2016-06-19 23:53:57 -0400

    Checkpoint 43 voting: completed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-<<<<<<< HEAD
 
   validates :body, length: { minimum: 5 }, presence: true
-  validates :user, presence: true 
-=======
->>>>>>> checkpoint-40-authorization
+  validates :user, presence: true
+
 end
