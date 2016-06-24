c1b6558b6b3efb8ae91b1352f5c73181d4776f58
KristenKarcher <kristenmkarcher@gmail.com>
2016-06-24 10:51:27 -0400

    Checkpoint 48 api

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
 
+  before_create :generate_auth_token
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
@@ -27,8 +29,15 @@ class User < ActiveRecord::Base
     favorites.where(post_id: post.id).first
   end
 
-  def avatar_url(size)
+  def self.avatar_url(user, size)
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,11 @@ end
   get 'welcome/contact'
 
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
